8b0b6443e3a7bbeb4e3922aa8b07464f80750309
XfensorX <97126540+XfensorX@users.noreply.github.com>
2026-03-19 22:32:02 +0800

    optimize phoenix efficiency

--- src/social_groups/trialrunner/utils/cli_utils.py
+++ src/social_groups/trialrunner/utils/cli_utils.py
@@ -38,7 +38,7 @@ def initialize_phoenix(conf: MainConfig, meta_info: ExperimentMetaInfo):
     if phoenix_server_is_up(url=f"{conf.execution.phoenix_server_url}/healthz"):
         log.info("Writing LLM Interactions into Phoenix. It is up and running.")
         setup_phoenix(
-            endpoint=conf.execution.phoenix_graphql_url,
+            endpoint=f"{conf.execution.phoenix_server_url}/v1/traces",
             project_name=meta_info.phoenix_project_name,
         )
 
