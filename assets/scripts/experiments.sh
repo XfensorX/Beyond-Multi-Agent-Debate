6129af8de567fa66d1a4969f12d653c8fc41ec17
XfensorX <97126540+XfensorX@users.noreply.github.com>
2026-04-15 17:21:56 +0800

    run more experiments

--- assets/scripts/experiments.sh
+++ assets/scripts/experiments.sh
@@ -1,26 +1,22 @@
 set -euo pipefail
 
 ITEMS=(
+# ----- Finished:
+
 # ------ Running:
-#'final/changed_order_mad2_qwen35'
-#'final/changed_order_mad2_ministral3'
+
+
+
+'final/changed_order_mad3_qwen3'
 
 
 # -------- To Do:
-#'final/changed_order_mad3_qwen3'
+
 #'final/changed_order_mad3_qwen35'
-
-#'final/baseline_structured_output_ministral3'
-#'final/changed_order_mad3_ministral3'
-
-#'final/no_discussion_voting_ministral3'
-#'final/no_discussion_voting_qwen35'
-#'final/no_discussion_voting_qwen3'
-
 
 )
 
-INTERVAL=10
+INTERVAL=20
 
 TIME=0
 for ITEM in "${ITEMS[@]}"; do
@@ -35,19 +31,31 @@ done
 
 
 # ------- Not Sorted:
-#'final/baseline_structured_output'
-#'final/baseline_structured_output_qwen35'
-#'final/changed_order_mad2_qwen3'
 
 
 # --------------------------- OLD ----------------------------
 
-#'final/baseline'
-#'final/baseline_tool'
-#'final/baseline_qwen35'
-#'final/baseline_tool_qwen35'
-#'final/baseline_ministral3'
 #'final/baseline_tool_ministral3'
 
+#'final/baseline'
+#'final/baseline_qwen35'
+#'final/baseline_ministral3'
+#'final/baseline_structured_output_ministral3'
 
+#'final/baseline_structured_output'
+#'final/baseline_structured_output_qwen35'
+#'final/baseline_tool'
+#'final/baseline_tool_qwen35'
+
+
+
+#'final/changed_order_mad2_qwen3'
+#'final/changed_order_mad2_ministral3'
+#'final/changed_order_mad2_qwen35'
+
+#'final/changed_order_mad3_ministral3'
+
+#'final/no_discussion_voting_qwen35'
+#'final/no_discussion_voting_qwen3'
+#'final/no_discussion_voting_ministral3'
 
--- assets/scripts/qwen35.sh
+++ assets/scripts/qwen35.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 ITEMS=(
 "Qwen/Qwen3.5-0.8B --gpus 1"
 "Qwen/Qwen3.5-4B --gpus 1"
-"Qwen/Qwen3.5-9B --gpus 1"
+"Qwen/Qwen3.5-9B --gpus 2"
 )
 
 
