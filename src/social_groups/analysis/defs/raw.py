0d460cde50a181a53aeec5560edbb081dd91c6e0
XfensorX <97126540+XfensorX@users.noreply.github.com>
2026-04-15 17:26:29 +0800

    add more support for data analysis, change input to layz frames, so creating the collective dataset is not as heavy 2

--- src/social_groups/analysis/defs/raw.py
+++ src/social_groups/analysis/defs/raw.py
@@ -1,6 +1,6 @@
 import dagster as dg
 import polars as pl
-from dagster import AssetKey, asset
+from dagster import AssetKey, FilesystemIOManager, asset
 from dagster_polars import PolarsParquetIOManager
 from dagstermill import ConfigurableLocalOutputNotebookIOManager
 
@@ -8,23 +8,23 @@ from social_groups.directories import DAGSTER_BASE_DIR, PARQUET_ANALYSIS_DIR
 
 
 @asset(key_prefix=["raw"], io_manager_key="polars_parquet_io_manager", group_name="raw")
-def answers() -> pl.DataFrame:
-    return pl.read_parquet(PARQUET_ANALYSIS_DIR / "Answer.parquet")
+def answers() -> pl.LazyFrame:
+    return pl.scan_parquet(PARQUET_ANALYSIS_DIR / "Answer.parquet")
 
 
 @asset(key_prefix=["raw"], io_manager_key="polars_parquet_io_manager", group_name="raw")
-def experiments() -> pl.DataFrame:
-    return pl.read_parquet(PARQUET_ANALYSIS_DIR / "Experiment.parquet")
+def experiments() -> pl.LazyFrame:
+    return pl.scan_parquet(PARQUET_ANALYSIS_DIR / "Experiment.parquet")
 
 
 @asset(key_prefix=["raw"], io_manager_key="polars_parquet_io_manager", group_name="raw")
-def questions() -> pl.DataFrame:
-    return pl.read_parquet(PARQUET_ANALYSIS_DIR / "Question.parquet")
+def questions() -> pl.LazyFrame:
+    return pl.scan_parquet(PARQUET_ANALYSIS_DIR / "Question.parquet")
 
 
 @asset(key_prefix=["raw"], io_manager_key="polars_parquet_io_manager", group_name="raw")
-def runs() -> pl.DataFrame:
-    return pl.read_parquet(PARQUET_ANALYSIS_DIR / "Run.parquet")
+def runs() -> pl.LazyFrame:
+    return pl.scan_parquet(PARQUET_ANALYSIS_DIR / "Run.parquet")
 
 
 @dg.asset(
@@ -38,16 +38,16 @@ def runs() -> pl.DataFrame:
     ],
 )
 def combined_data(
-    answers: pl.DataFrame,
-    runs: pl.DataFrame,
-    experiments: pl.DataFrame,
-    questions: pl.DataFrame,
+    answers: pl.LazyFrame,
+    runs: pl.LazyFrame,
+    experiments: pl.LazyFrame,
+    questions: pl.LazyFrame,
 ) -> pl.DataFrame:
     return (
         answers.join(runs, on="run_id", how="left")
         .join(experiments, on="experiment_id", how="left")
         .join(questions, on="question_id", how="left")
-    )
+    ).collect()
 
 
 defs = dg.Definitions(
@@ -61,5 +61,6 @@ defs = dg.Definitions(
         "output_notebook_io_manager": ConfigurableLocalOutputNotebookIOManager(
             base_dir=str(DAGSTER_BASE_DIR)
         ),
+        "io_manager": FilesystemIOManager(base_dir=str(DAGSTER_BASE_DIR)),
     },
 )
