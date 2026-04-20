8a2b87e42a22e95c5e6c25f57278f923dbdec3c4
XfensorX <97126540+XfensorX@users.noreply.github.com>
2026-04-20 18:43:40 +0700

    implement all mad experiments on the smaller subset and for all model families

--- assets/scripts/experiments.sh
+++ assets/scripts/experiments.sh
@@ -2,17 +2,70 @@ set -euo pipefail
 
 ITEMS=(
 # ----- Finished:
+#'final/baseline'
+#'final/baseline_structured_output'
+#'final/baseline_tool'
 
 # ------ Running:
 
-
-
-'final/changed_order_mad3_qwen3'
-
+#
+#'final/changed_order_mad2_qwen3'
+#'final/changed_order_mad3_qwen3'
+#'final/no_discussion_voting_qwen3'
 
 # -------- To Do:
+#'final/baseline_tool_ministral3'
+#'final/baseline_ministral3'
+#'final/baseline_structured_output_ministral3'
 
-#'final/changed_order_mad3_qwen35'
+#'final/changed_order_mad2_ministral3'
+#'final/no_discussion_voting_ministral3'
+#'final/changed_order_mad3_ministral3'
+
+
+# -----
+'final/changed_prompt_mad2_qwen3'
+'final/changed_prompt_mad2_ministral3'
+'final/changed_prompt_mad2_qwen35'
+
+
+'final/thinking_mad2_qwen3'
+'final/thinking_mad2_ministral3'
+'final/thinking_mad2_qwen35'
+
+'final/changed_prompt_mad3_qwen3'
+'final/changed_prompt_mad3_ministral3'
+'final/changed_prompt_mad3_qwen35'
+
+
+'final/thinking_mad3_qwen3'
+'final/thinking_mad3_ministral3'
+'final/thinking_mad3_qwen35'
+
+
+'final/small_mad1_qwen3'
+'final/small_mad1_ministral3'
+'final/small_mad1_qwen35'
+
+'final/small_mad2_qwen3'
+'final/small_mad2_ministral3'
+'final/small_mad2_qwen35'
+
+'final/small_mad3_qwen3'
+'final/small_mad3_ministral3'
+'final/small_mad3_qwen35'
+
+'final/small_mad4_qwen3'
+'final/small_mad4_ministral3'
+'final/small_mad4_qwen35'
+
+'final/diversity_param_sweep_mad2_qwen3'
+'final/diversity_param_sweep_mad2_ministral3'
+'final/diversity_param_sweep_mad2_qwen35'
+
+'final/diversity_param_sweep_mad3_qwen3'
+'final/diversity_param_sweep_mad3_ministral3'
+'final/diversity_param_sweep_mad3_qwen35'
 
 )
 
@@ -31,31 +84,13 @@ done
 
 
 # ------- Not Sorted:
-
+#'final/baseline_qwen35'
+#'final/baseline_structured_output_qwen35'
+#'final/baseline_tool_qwen35'
+#'final/no_discussion_voting_qwen35'
+#'final/changed_order_mad2_qwen35'
+#'final/changed_order_mad3_qwen35'
 
 # --------------------------- OLD ----------------------------
 
-#'final/baseline_tool_ministral3'
-
-#'final/baseline'
-#'final/baseline_qwen35'
-#'final/baseline_ministral3'
-#'final/baseline_structured_output_ministral3'
-
-#'final/baseline_structured_output'
-#'final/baseline_structured_output_qwen35'
-#'final/baseline_tool'
-#'final/baseline_tool_qwen35'
-
-
-
-#'final/changed_order_mad2_qwen3'
-#'final/changed_order_mad2_ministral3'
-#'final/changed_order_mad2_qwen35'
-
-#'final/changed_order_mad3_ministral3'
-
-#'final/no_discussion_voting_qwen35'
-#'final/no_discussion_voting_qwen3'
-#'final/no_discussion_voting_ministral3'
 
