6ca9049c3787a051469dc6e51fd1c25998bd7259
XfensorX <97126540+XfensorX@users.noreply.github.com>
2026-06-27 00:36:12 +0200

    add more lgos in tribal

--- src/social_groups/trialrunner/decision_schemes/tribal_council.py
+++ src/social_groups/trialrunner/decision_schemes/tribal_council.py
@@ -1,3 +1,4 @@
+import json
 import re
 from functools import cached_property
 from typing import Annotated, Literal
@@ -142,7 +143,18 @@ def make_proposal(
     )
 
     args = parse_tool_call_arguments(ai_msg)
-    parser = AnswerParser(AnswerOptions.letters_A_to_J)
+    with phoenix_log_span(
+        "Received Invalid Tool Call",
+        title="Aborting",
+        attrs={
+            "args_of_tool_call": json.dumps(args),
+            "ai_message": ai_msg.content,
+            "cannot_choose": cannot_choose,
+            "question": question,
+        },
+    ):
+        parser = AnswerParser(AnswerOptions.letters_A_to_J)
+
     try:
         return Proposal(
             answer=parser._parser(args["correct_answer"]),
